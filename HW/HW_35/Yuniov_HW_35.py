48865bb765c8693e6c0a105f748c8bd8471b7d88
intelligent-blr <a.yuniov@gmail.com>
2025-03-16 23:38:53 +0100

    Update

---
+++ HW/HW_35/Yuniov_HW_35.py
@@ -0,0 +1,25 @@
+import requests
+
+# Напишите функцию get_response(url), которая отправляет GET-запрос
+# по заданному URL-адресу и возвращает объект ответа.
+# Затем выведите следующую информацию об ответе:
+# - Код состояния(status code)
+# - Текст ответа(response text)
+# - Заголовки ответа(response headers)
+
+
+def get_response(url):
+    response = requests.get("https://"+url)
+    print(response.status_code)
+    print(response.text)
+    print(response.headers)
+
+
+url = input("Введите адрес сайта: ")
+
+get_response(url)
+
+if response:
+    print(f"Status Code: {response.status_code}")
+    print(f"Response Text: {response.text}")
+    print(f"Response Headers: {response.headers}")
