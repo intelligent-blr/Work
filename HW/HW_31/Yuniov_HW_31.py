8676a6054c6f005994c6c4245645897eb4c98b05
intelligent-blr <a.yuniov@gmail.com>
2025-03-17 18:21:30 +0100

    Update

--- HW/HW_31/Yuniov_HW_31.py
+++ HW/HW_31/Yuniov_HW_31.py
@@ -1,3 +1,5 @@
+import logging
+
 # 1. Напишите декоратор validate_args, который будет проверять типы аргументов
 # функции и выводить сообщение об ошибке, если переданы аргументы неправильного
 # типа. Декоратор должен принимать ожидаемые типы аргументов в качестве
@@ -28,3 +30,22 @@ example_function(11, "Hello")
 # результаты вызовов функции в лог-файл. Каждый вызов функции должен быть
 # записан на новой строке в формате "Аргументы: <аргументы>,
 # Результат: <результат>". Используйте модуль logging для записи в лог-файл.
+
+logging.basicConfig(filename='log.txt', level=logging.INFO, encoding='utf-8')
+
+
+def log_args(func):
+    def wrapper(*args, **kwargs):
+        logging.info(f'Аргументы: {args}, {kwargs}')
+        result = func(*args, **kwargs)
+        logging.info(f'Результат: {result}')
+        return result
+    return wrapper
+
+
+@log_args
+def add(a, b):
+    return a + b
+
+
+add(4, 3)
--- Lessen/practikum.py
+++ Lessen/practikum.py
@@ -1,25 +1,34 @@
-def validate_args(*expected_types):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            # Проверяем типы аргументов
-            for i, (arg, expected_type) in enumerate(zip(args, expected_types)):
-                if not isinstance(arg, expected_type):
-                    print(f"Ошибка: аргумент {i + 1} должен быть типа {expected_type.__name__}, а не {type(arg).__name__}")
-                    return
-            # Если типы корректные, вызываем оригинальную функцию
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
+import logging
+
+# Конфигурация логгера
+logging.basicConfig(filename='function_calls.log',
+                    level=logging.INFO,
+                    format='%(asctime)s - %(message)s')
+
+def log_args(func):
+    def wrapper(*args, **kwargs):
+        # Записываем аргументы в лог
+        logging.info(f'Аргументы: {args}, {kwargs}')
+
+        # Вызов функции и получение результата
+        result = func(*args, **kwargs)
+
+        # Записываем результат в лог
+        logging.info(f'Результат: {result}')
+
+        return result
+    return wrapper
 
 # Пример использования декоратора
 
-@validate_args(int, str)
-def example_function(a, b):
-    print(f"Аргумент 1: {a}, Аргумент 2: {b}")
+@log_args
+def add(a, b):
+    return a + b
 
-# Пример с правильными типами
-example_function(42, "Hello")
+@log_args
+def greet(name):
+    return f'Hello, {name}!'
 
-# Пример с неправильными типами
-example_function("42", "Hello")
-example_function(42, 100)
+# Вызовем функции для примера
+add(3, 5)
+greet('Alice')
