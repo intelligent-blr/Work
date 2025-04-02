9b24edcfaf3f7a992953668c3b1561d9d16e3d66
intelligent-blr <a.yuniov@gmail.com>
2025-04-02 19:56:18 +0200

    Update

--- Lessen/Lesson_26/step_2.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-print('Список параметров, переданных скрипту')
-print(sys.argv)
-
-print([arg for arg in sys.argv if arg[0]!='-'])
--- Lessen/Lesson_37/example.py
+++
@@ -1,5 +0,0 @@
-name = "Alice"
-age = 30
-message = "Привет, меня зовут %s и мне %s лет." % (name, age)
-
-print(message)
