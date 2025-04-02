0b6e7142a1f6efd5ae9575cf5b865a1911b34839
intelligent-blr <a.yuniov@gmail.com>
2025-04-02 20:00:06 +0200

    Update

---
+++ Project/config.py
@@ -0,0 +1,15 @@
+dbconfig_edit = {'host': '',
+                 'user': '',
+                 'password': '',
+                 }
+
+dbconfig_read = {'host': '',
+                 'user': '',
+                 'password': ''
+                 }
+
+my_base = "Yuniou_300924"
+
+find_base = "sakila"
+
+file_dir = r"D:\ICH\_Python Fundamentals\Work\Work\Project\stop_words.txt"
